3ce1f3bb1753ab2e8b75d9b27774789d7fc38d13
Erackson Souza <souza.erackson@gmail.com>
2025-03-11 20:43:39 -0300

    bomb commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{html,js,jsx,tsx}"],
+
+  theme: {
+    fontFamily: {
+      'poppins': ['Poppins', 'sans-serif'],
+    }
+    extend: {},
+  },
+  plugins: [],
+}
+
